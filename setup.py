e170e8c918dc3fc62bb33efaad9ffa129ce0047b
Miguel Rivera <miguel.rivera@hotmail.fr>
2021-04-05 21:31:55 +0100

    add project skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='kjb',
+      description='Bible verse generator',
+      author='Arthur Scammell',
+      author_email='artscam@gmail.com',
+      packages=['kjb',
+                'kjb.src']
+      )
